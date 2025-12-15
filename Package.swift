c0295aacf8fc687ef15bbf336e004b41ed61a25e
djarry <damien.jarry@wengo.com>
2025-12-15 12:52:31 +0100

    [WIP] Add missing iOS deps

--- Package.swift
+++ Package.swift
@@ -10,14 +10,16 @@ let package = Package(
             targets: ["RiveSplashScreenPlugin"])
     ],
     dependencies: [
-        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0")
+        .package(url: "https://github.com/ionic-team/capacitor-swift-pm.git", from: "7.0.0"),
+        .package(url: "https://github.com/rive-app/rive-ios.git", from: "6.0.0")
     ],
     targets: [
         .target(
             name: "RiveSplashScreenPlugin",
             dependencies: [
                 .product(name: "Capacitor", package: "capacitor-swift-pm"),
-                .product(name: "Cordova", package: "capacitor-swift-pm")
+                .product(name: "Cordova", package: "capacitor-swift-pm"),
+                .product(name: "RiveRuntime", package: "rive-ios")
             ],
             path: "ios/Sources/RiveSplashScreenPlugin"),
         .testTarget(
